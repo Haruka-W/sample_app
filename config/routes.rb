816b80287511c5909ff6815730a521bf42088f9f
Haruka-W <nekonoko1995@gmail.com>
2020-02-08 18:46:12 +0900

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
 
-  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolidt'
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 
 end
