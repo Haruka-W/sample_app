5313ab68fd47bcb9f5eb7ff957c72e149d465b40
Haruka-W <nekonoko1995@gmail.com>
2020-02-08 18:00:12 +0900

    [Start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'todolists/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -10,5 +11,7 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolidt'
 
 end
